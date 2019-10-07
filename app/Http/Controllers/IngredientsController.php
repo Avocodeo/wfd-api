9f606a570f98039fa28aa50e308c7b177fdcb18b
James Moore <jamesm0025@gmail.com>
2019-10-06 21:09:53 -0400

    Example test, model, view and controller for an unauthenticated user creating a ingredient

--- app/Http/Controllers/IngredientsController.php
+++ app/Http/Controllers/IngredientsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Ingredient;
 
 class IngredientsController extends Controller
 {
@@ -10,17 +11,29 @@ class IngredientsController extends Controller
     {
 
     }
+
     public function show()
     {
 
     }
+
     public function create()
     {
         return view('ingredients.create');
     }
+
     public function store()
     {
+        $attributes = request()->validate([
+            'name' => 'required',
+            'measurement_id' => 'required',
+        ]);
 
+        $ingredient = Ingredient::create($attributes);
+        return response()->json([
+            'id' => $ingredient->id,
+            'message' => 'ingredient was successfully created'
+        ]);
     }
 
     public function update()
