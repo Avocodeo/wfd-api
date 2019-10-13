32b7dd36a1e08aa14674c5263739b42047cdc5f6
James Moore <jamesm0025@gmail.com>
2019-10-13 12:06:47 -0400

    categories table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(MeasurementsTableSeeder::class);
         $this->call(IngredientsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
     }
 }
