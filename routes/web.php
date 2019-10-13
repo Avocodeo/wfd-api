064f63340b329a27cb56c89d8ffa84a78d210df2
James Moore <jamesm0025@gmail.com>
2019-10-13 11:57:33 -0400

    Categories controller, model, migration and tests

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/ingredients/create', 'IngredientsController@create');
 Route::get('/measurements/create', 'MeasurementsController@create');
+Route::get('/categories/create', 'CategoriesController@create');
