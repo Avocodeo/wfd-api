60f78340611f2c057d583c61f6427ec8e9659848
Tyler <ouell117@uwindsor.ca>
2019-10-21 14:18:03 -0400

    fixed inventory tab, maybe that was the error?

--- app/Inventory.php
+++ app/Inventory.php
@@ -1,17 +1,19 @@
- <?php
+<?php
 
-    use Illuminate\Database\Eloquent\Model;
+namespace App;
 
-    class Inventory extends Model
+use Illuminate\Database\Eloquent\Model;
+
+class Inventory extends Model
+{
+    protected $fillable = ['item_id', 'item_quantity', 'item_low', 'item_high', 'item_close_to_expiry'];
+
+    protected $guarded = [];
+
+    protected $with = ['inventory'];
+
+    public function inventory()
     {
-        protected $fillable = ['item_id', 'item_quantity', 'item_low', 'item_high', 'item_close_to_expiry'];
-
-        protected $guarded = [];
-
-        protected $with = ['inventory'];
-
-        // public function inventory()
-        // {
-        //     return $this->belongsTo(Inventory::class);
-        // }
+        return $this->belongsTo(Inventory::class);
     }
+}
