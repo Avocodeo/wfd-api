fd3ff405f34a5873e779464c459eb4e26d3f1eb0
Justin Dearden <deardenj@uwindsor.ca>
2019-10-20 23:13:29 -0400

    Added Recipes Management Page.

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 class RecipesController extends Controller
 {
+    public function index()
+    {
+        return view('recipes.index');
+    }
+
     public function create()
     {
         return view('recipes.create');
