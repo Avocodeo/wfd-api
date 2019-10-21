26c0fbbd682b70306e588ebae811c7aa412d8459
Tyler <ouell117@uwindsor.ca>
2019-10-21 16:35:38 -0400

    Touched  up Inventory Vue for quantity, high, low etc

--- resources/js/views/Inventories.vue
+++ resources/js/views/Inventories.vue
@@ -1,62 +1,62 @@
 <template>
-    <v-card>
-      <v-card-title>
-        Inventory
-        <v-spacer></v-spacer>
-        <v-text-field
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Search"
-          single-line
-          hide-details
-        ></v-text-field>
-      </v-card-title>
-      <v-data-table
-        :headers="headers"
-        :items="inventories"
-        :search="search"
-        :loading="loading"
-        loading-text="Loading Inventory... Please wait"
-      >
-        <template v-slot:item.action="{ item }">
-          <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
-          <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
-        </template>
-        <template v-slot:no-data>
-          <v-btn color="primary" @click="initialize">Reset</v-btn>
-        </template>
-      </v-data-table>
-      <v-dialog v-model="dialog" max-width="500px">
-        <template v-slot:activator="{ on }">
-          <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
-        </template>
-        <v-card>
-          <v-card-title>
-            <span class="headline">{{ formTitle }}</span>
-          </v-card-title>
+  <v-card>
+    <v-card-title>
+      Inventory
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="inventories"
+      :search="search"
+      :loading="loading"
+      loading-text="Loading Inventory... Please wait"
+    >
+      <template v-slot:item.action="{ item }">
+        <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+        <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="initialize">Reset</v-btn>
+      </template>
+    </v-data-table>
+    <v-dialog v-model="dialog" max-width="500px">
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{ formTitle }}</span>
+        </v-card-title>
 
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col cols="12" md="6">
-                  <v-text-field v-model="editedItem.name" label="Item Name"></v-text-field>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card-text>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" md="6">
+                <v-text-field v-model="editedItem.name" label="Item Name"></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
 
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-            <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-      <v-snackbar v-model="snackbar" :timeout="snackbarTimeout">
-        {{ snackbarText }}
-        <v-btn color="blue" text @click="snackbar = false">Close</v-btn>
-      </v-snackbar>
-    </v-card>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+          <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-snackbar v-model="snackbar" :timeout="snackbarTimeout">
+      {{ snackbarText }}
+      <v-btn color="blue" text @click="snackbar = false">Close</v-btn>
+    </v-snackbar>
+  </v-card>
 </template>
 
 <script>
@@ -66,11 +66,14 @@ export default {
       search: "",
       headers: [
         {
-          text: "Item Name",
+          text: "Id",
           align: "left",
           sortable: false,
-          value: "name"
+          value: "id"
         },
+        { text: "Quantity", value: "quantity" },
+        { text: "Low", value: "low" },
+        { text: "High", value: "high" },
         { text: "Created At", value: "created_at" },
         { text: "Updated At", value: "updated_at" },
         { text: "Actions", value: "action", sortable: false }
@@ -154,8 +157,10 @@ export default {
         this.snackbar = true;
         axios
           .patch("api/inventories/" + this.editedItem.id, {
-            name: this.editedItem.name,
-            inventory_id: this.editedItem.inventory.id
+            id: this.editedItem.id,
+            quantity: this.editedItem.inventory.quantity,
+            low: this.editedItem.low,
+            high: this.editedItem.high
           })
           .then(function(response) {
             console.log(response);
@@ -163,15 +168,19 @@ export default {
       } else {
         axios
           .post("api/inventories", {
-            name: this.editedItem.name,
-            inventory_id: this.editedItem.inventory.id
+            id: this.editedItem.id,
+            quantity: this.editedItem.inventory.quantity,
+            low: this.editedItem.low,
+            high: this.editedItem.high
           })
           .then(function(response) {
             console.log(response);
           });
         this.ingredients.push({
-          name: this.editedItem.name,
-          "inventory.name": this.editedItem.inventory.name
+          id: this.editedItem.id,
+          quantity: this.editedItem.inventory.quantity,
+          low: this.editedItem.low,
+          high: this.editedItem.high
         });
         this.snackbar = true;
         this.snackbarText = "Item created";
