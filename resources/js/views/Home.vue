a1b55c546b637e08b3480cacbfe2fe8a89ff096c
Tyler <ouell117@uwindsor.ca>
2019-11-08 13:12:03 -0500

    added redirect to groceries list on homepage button

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,17 +1,9 @@
 <template>
   <v-container fluid fill-height>
-    <v-layout
-      align-center
-      justify-center
-    >
+    <v-layout align-center justify-center>
       <v-flex>
-
         <v-card class="elevation-12 mb-6">
-          <v-toolbar
-                  color="primary"
-                  dark
-                  flat
-          >
+          <v-toolbar color="primary" dark flat>
             <v-toolbar-title>Groceries</v-toolbar-title>
             <v-spacer></v-spacer>
           </v-toolbar>
@@ -21,16 +13,12 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary">List</v-btn>
+            <v-btn color="primary" to="/groceries">List</v-btn>
           </v-card-actions>
         </v-card>
 
         <v-card class="elevation-12">
-          <v-toolbar
-                  color="primary"
-                  dark
-                  flat
-          >
+          <v-toolbar color="primary" dark flat>
             <v-toolbar-title>Things You Can Make</v-toolbar-title>
             <v-spacer></v-spacer>
           </v-toolbar>
@@ -43,7 +31,6 @@
             <v-btn color="primary">Meals</v-btn>
           </v-card-actions>
         </v-card>
-
       </v-flex>
     </v-layout>
   </v-container>
