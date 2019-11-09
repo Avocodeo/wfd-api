d10409f4ac06336afd5ce8f31422be4530c98992
Tyler <ouell117@uwindsor.ca>
2019-11-08 20:40:10 -0500

    added hard coded componenet to home page for high level use case

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -4,6 +4,26 @@
       <v-flex>
         <v-card class="elevation-12 mb-6">
           <v-toolbar color="primary" dark flat>
+            <v-toolbar-title>Suppliers</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+          <v-card-text>
+            <strong>
+              <h2>This is a list of our top suppliers.</h2>
+              <h3>
+                <br />1. Tromp-Gleichner
+                <br />2. Gerlach-Luettgen
+                <br />3. Hagenes Group
+              </h3>
+            </strong>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" to="/suppliers">Rest of List</v-btn>
+          </v-card-actions>
+        </v-card>
+        <v-card class="elevation-12 mb-6">
+          <v-toolbar color="primary" dark flat>
             <v-toolbar-title>Groceries</v-toolbar-title>
             <v-spacer></v-spacer>
           </v-toolbar>
@@ -36,11 +56,126 @@
   </v-container>
 </template>
 
+
 <script>
 export default {
-  name: "Home"
+  data() {
+    return {
+      search: "",
+      headers: [
+        {
+          text: "id",
+          align: "left",
+          sortable: false,
+          value: "id"
+        },
+        { text: "Name", value: "name" },
+        { text: "Type", value: "type" },
+        { text: "Created At", value: "created_at" },
+        { text: "Updated At", value: "updated_at" },
+        { text: "Actions", value: "action", sortable: false }
+      ],
+      suppliers: [],
+      editedIndex: -1,
+      editedItem: {
+        name: "",
+        type: ""
+      },
+      defaultItem: {
+        name: "",
+        type: ""
+      },
+      loading: true,
+      dialog: false,
+      snackbar: false,
+      snackbarText: "",
+      snackbarTimeout: 2000
+    };
+  },
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Supplier" : "Edit Supplier";
+    }
+  },
+  watch: {
+    dialog(val) {
+      val || this.close();
+    }
+  },
+  created() {
+    this.getSupplier();
+  },
+  methods: {
+    getSupplier: function() {
+      axios
+        .get("api/suppliers")
+        .then(response => {
+          this.suppliers = response.data;
+          this.loading = false;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+
+    editItem(item) {
+      this.editedIndex = this.suppliers.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      const index = this.suppliers.indexOf(item);
+      if (confirm("Are you sure you want to delete this supplier?")) {
+        if (this.suppliers.splice(index, 1)) {
+          axios.delete("api/suppliers/" + item.id);
+          this.snackbarText = "Supplier deleted";
+          this.snackbar = true;
+        }
+      }
+    },
+
+    close() {
+      this.dialog = false;
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.suppliers[this.editedIndex], this.editedItem);
+        this.snackbarText = "Supplier updated";
+        this.snackbar = true;
+        axios
+          .patch("api/suppliers/" + this.editedItem.id, {
+            name: this.editedItem.name
+          })
+          .then(function(response) {
+            console.log(response);
+          });
+      } else {
+        axios
+          .post("api/suppliers", {
+            name: this.editedItem.name
+          })
+          .then(function(response) {
+            console.log(response);
+          });
+        this.suppliers.push({
+          name: this.editedItem.name,
+          "supplier.name": this.editedItem.supplier.name
+        });
+        this.snackbar = true;
+        this.snackbarText = "Supplier created";
+      }
+      this.close();
+    }
+  }
 };
 </script>
+
 
 <style lang="scss" scoped>
 </style>
